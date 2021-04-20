e4f6a8413a573ab4e5919304cbaaa62b05ff69ae
Huriel Lopes <huriel-lopes@hotmail.com>
2021-04-20 13:45:22 -0300

    Finalização da aula 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
+import { useEffect } from "react";
+
+export default function Home(props) {
   return <h1>index</h1>;
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
